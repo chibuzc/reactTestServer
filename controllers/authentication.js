992e40aa8c02a427bd98cf1e72272e6daf979af7
Charles <chibuzc@yahoo.co.uk>
2017-09-05 12:34:35 +0100

    first commit

---
+++ controllers/authentication.js
@@ -0,0 +1,47 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user){
+	const timestamp = new Date().getTime();
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+exports.signin = function(req, res, next){
+	console.log('also here')
+	res.send( {token: tokenForUser(req.user) } )
+}
+
+
+exports.signup = function(req, res, next){
+	console.log(req.body)
+	const email = req.body.email;
+	const password = req.body.password;
+	//see if a user with the given email exists
+	User.findOne({ email: email }, function(err, existingUser){
+		if(err){ return next(err); }
+	
+		if(!email || !password){
+			return res.status(422).send({error: "Ypu must provide username and password"})
+		}
+	//if user with email does exist, return an error
+		if (existingUser){
+			return res.status(422).send({error: 'Email is in use' });
+		}
+
+	//if user with email does not exist, create and save user
+		const user = new User({
+			email: email,
+			password: password
+		})
+		user.save(function(err){
+			if(err){ return next(err); }
+			//respond to request indicating the user was created
+			res.json({ token: tokenForUser(user) })
+
+		})
+			
+		
+	});
+
+	
+}
